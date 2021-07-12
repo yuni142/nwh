bcb4598fc3440a469145427415a33f583e418f5d
yuni142 <yuninur142@gmail.com>
2021-07-12 21:20:51 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderNwh/>
+    <HeroNwh/>
+    <WomenNwh/>
+    <InstaNwh/>
+    <PartnerNwh/>
+    <FooterNwh/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderNwh from '@/components/HeaderNwh.vue';
+import HeroNwh from '@/components/HeroNwh.vue';
+import WomenNwh from '@/components/WomenNwh.vue';
+import InstaNwh from '@/components/InstaNwh.vue';
+import PartnerNwh from '@/components/PartnerNwh.vue';
+import FooterNwh from '@/components/FooterNwh.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderNwh,
+    HeroNwh,
+    WomenNwh,
+    InstaNwh,
+    PartnerNwh,
+    FooterNwh
+  }
+}
+</script>
